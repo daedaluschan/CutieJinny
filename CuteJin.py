37e3711ee9769d3c82244c22ab16911f3cd28aeb
daedalus <daedalus.chan@gmail.com>
2017-06-03 00:55:19 +0800

    Included the scheduled sending into same the main program

--- CuteJin.py
+++ CuteJin.py
@@ -7,11 +7,9 @@ from telegram import replykeyboardmarkup
 from telegram import replykeyboardremove
 import logging
 from logging.handlers import RotatingFileHandler
-from datetime import date
+from datetime import date, time
 from re import compile, match
-
-log_file_size_lmt = 10485760
-log_file_count_lmt = 5
+from CuteJin_cfg import *
 
 handler =  RotatingFileHandler(filename="logs/CutieJinny.log", maxBytes=log_file_size_lmt, backupCount=log_file_count_lmt)
 handler.setFormatter(logging.Formatter('%(asctime)s | %(name)s | %(levelname)s | %(message)s'))
@@ -20,11 +18,11 @@ logging.getLogger().addHandler(handler)
 logging.getLogger().setLevel(logging.DEBUG)
 
 
-TOKEN = sys.argv[1]  # get token from command-line
-file_name = sys.argv[2]
-photo_base_dir = sys.argv[3]
+# TOKEN = sys.argv[1]  # get token from command-line
+# file_name = sys.argv[2]
+# photo_base_dir = sys.argv[3]
 
-def send_photo(bot, update):
+def send_photo(bot, recipient):
     with open(file=file_name, mode='r') as f:
         line = f.read()
     f.close()
@@ -32,17 +30,14 @@ def send_photo(bot, update):
 
     logging.info("photo_path : {}".format(photo_path))
 
-    bot.sendPhoto(chat_id=update.message.chat_id, photo=open(photo_path, 'rb'))
+    bot.sendPhoto(chat_id=recipient, photo=open(photo_path, 'rb'))
 
 
 
 updater = Updater(token=TOKEN)
 dispatcher = updater.dispatcher
 
-
 from functools import wraps
-
-LIST_OF_ADMINS = [161517202, 197627552]
 
 def restricted(func):
     @wraps(func)
@@ -87,45 +82,59 @@ def start(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id, text="你好，這是可愛小 Jinny Telegram 機械人。",
                     reply_markup=markup)
 
+def pick_photo_to_send(bot, to_list):
+    try:
+        dir_list = get_immediate_subdirectories(photo_base_dir)
+        size = len(dir_list)
+        picked_index = randint(0, size - 1)
+        picked_dir = photo_base_dir + "/" + dir_list[picked_index]
+        logging.info("Picked Dir : {}".format(picked_dir))
+
+        sub_dir_list = get_immediate_subdirectories(picked_dir)
+        sub_dir_size = len(sub_dir_list)
+        picked_sub_index = randint(0, sub_dir_size - 1)
+        picked_sub_dir = picked_dir + "/" + sub_dir_list[picked_sub_index]
+        logging.info("Picked Sub Dir : {}".format(picked_sub_dir))
+
+        photo_list = get_all_jpg(picked_sub_dir)
+        num_of_photo = len(photo_list)
+        picked_photo_index = randint(0, num_of_photo - 1)
+        picked_photo = picked_sub_dir + "/" + photo_list[picked_photo_index]
+        logging.info("Picked PHOTO : {}".format(picked_photo))
+
+        with open(file_name, mode='w') as f:
+            f.write(picked_photo)
+
+        photo_desc = compile('.*\/(\d\d\d\d_\d\d_\d\d\@Day[^/]+)\/.*').match(picked_photo).group(1)
+        logging.info("Photo_info: {}".format(photo_desc))
+
+        for recipient in to_list:
+            send_photo(bot=bot, recipient=recipient)
+            bot.sendMessage(chat_id=recipient, text=msg_daily_photo.format(photo_desc))
+
+    except(ValueError):
+        for recipient in to_list:
+            bot.sendMessage(chat_id=recipient, text="Load 唔到相呀。再試過啦。")
+
+
 @restricted
 def handleMsg(bot, update):
     if(update.message.text == "猩相"):
-        send_photo(bot=bot, update=update)
+        send_photo(bot=bot, recipient=update.message.from_user.id)
         # bot.sendPhoto(chat_id=update.message.chat_id, photo=open(photo_path, 'rb'))
         bot.sendMessage(chat_id=update.message.chat_id, text="SENT", reply_markup=markup)
     elif(update.message.text == "換相"):
-        try:
-            dir_list = get_immediate_subdirectories(photo_base_dir)
-            size = len(dir_list)
-            picked_index = randint(0, size - 1)
-            picked_dir = photo_base_dir + "/" + dir_list[picked_index]
-            logging.info("Picked Dir : {}".format(picked_dir))
-
-            sub_dir_list = get_immediate_subdirectories(picked_dir)
-            sub_dir_size = len(sub_dir_list)
-            picked_sub_index = randint(0, sub_dir_size - 1)
-            picked_sub_dir = picked_dir + "/" + sub_dir_list[picked_sub_index]
-            logging.info("Picked Sub Dir : {}".format(picked_sub_dir))
-
-            photo_list = get_all_jpg(picked_sub_dir)
-            num_of_photo = len(photo_list)
-            picked_photo_index = randint(0, num_of_photo - 1)
-            picked_photo = picked_sub_dir + "/" + photo_list[picked_photo_index]
-            logging.info("Picked PHOTO : {}".format(picked_photo))
-
-            with open(file_name, mode='w') as f:
-                f.write(picked_photo)
-
-            send_photo(bot=bot, update=update)
-
-            photo_desc = compile('.*\/(\d\d\d\d_\d\d_\d\d\@Day[^/]+)\/.*').match(picked_photo).group(1)
-            logging.info("Photo_info: {}".format(photo_desc))
-            bot.sendMessage(chat_id=update.message.chat_id, text="INFO: {}".format(photo_desc))
-
-        except(ValueError):
-            bot.sendMessage(chat_id=update.message.chat_id, text="Load 唔到相呀。再試過啦。")
+        pick_photo_to_send(bot=bot, to_list=[update.message.from_user.id])
     else:
         bot.sendMessage(chat_id=update.message.chat_id, text="跟人講：" + update.message.text, reply_markup=markup)
+
+def daily_photo(bot, job):
+    pick_photo_to_send(bot=bot, to_list=LIST_OF_ADMINS)
+
+
+jq = updater.job_queue
+jq.run_daily(callback=daily_photo, time=time(hour=schedule_time_hh_1, minute=schedule_time_mm_1))
+jq.run_daily(callback=daily_photo, time=time(hour=schedule_time_hh_2, minute=schedule_time_mm_2))
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
@@ -133,4 +142,4 @@ dispatcher.add_handler(start_handler)
 echo_handler = MessageHandler(Filters.text, handleMsg)
 dispatcher.add_handler(echo_handler)
 
-updater.start_polling()
+updater.start_polling(timeout=30)
---
+++ CuteJin_cfg.py
@@ -0,0 +1,17 @@
+log_file_size_lmt = 10485760
+log_file_count_lmt = 5
+
+
+TOKEN = ""
+file_name = ""
+photo_base_dir = ""
+
+LIST_OF_ADMINS = []
+
+schedule_time_hh_1 = 10
+schedule_time_mm_1 = 00
+
+schedule_time_hh_2 = 15
+schedule_time_mm_2 = 15
+
+msg_daily_photo = "今日靚相精選：{}"
